57bc996f871df28690d57921298f6fd17c007b16
kelviny01 <kelviny.mk27@gmail.com>
2024-06-15 00:33:52 -0300

    add: action of searching for a player

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,2 +1,51 @@
 "use strict";
-console.log("Testando...");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const containerSports = document.getElementById("container-sports");
+const inputSearch = document.querySelector("#input-search input");
+const buttonSearchPlayer = document.getElementById("button-search-player");
+const containerPlayer = document.getElementById("container-player");
+const playerImage = document.querySelector("#player-image img");
+const playerCharacteristics = document.querySelectorAll("#player-characteristics span strong");
+const playerName = document.getElementById("player-name");
+const playerNumber = document.getElementById("player-number");
+const playerAge = document.getElementById("player-age");
+const playerTeam = document.getElementById("player-team");
+function active() {
+    containerSports.classList.toggle("active");
+}
+const API_URL = "https://apiv3.apifootball.com/";
+const API_KEY = "7a12383b8a187e43f23ef4b4dc0157c0a0018317478883d63a92b2c47dde2426";
+function fetchDataPlayer(namePlayer) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch(`${API_URL}?action=get_players&player_name=${namePlayer}&APIkey=${API_KEY}`);
+            const data = yield response.json();
+            if (data.error === 404)
+                return;
+            return data;
+        }
+        catch (_a) {
+            console.log("Some error!");
+        }
+    });
+}
+buttonSearchPlayer.addEventListener("click", () => {
+    fetchDataPlayer(inputSearch.value).then((data) => {
+        let playerData = data;
+        playerData.forEach((item) => {
+            playerImage.src = item.player_image;
+            playerName.textContent = item.player_name;
+            playerNumber.textContent = item.player_number;
+            playerAge.textContent = item.player_age;
+            playerTeam.textContent = item.team_name;
+        });
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,73 @@
-console.log("Testando...")
+// https://apiv3.apifootball.com/?action=get_players&player_name=Benzema&APIkey=7a12383b8a187e43f23ef4b4dc0157c0a0018317478883d63a92b2c47dde2426
+
+interface IPlayer {
+  player_image: string;
+  player_name: string;
+  player_number: string;
+  player_age: string;
+  team_name: string;
+}
+
+// Select Elements:
+const containerSports = document.getElementById(
+  "container-sports"
+) as HTMLDivElement;
+const inputSearch = document.querySelector(
+  "#input-search input"
+) as HTMLInputElement;
+const buttonSearchPlayer = document.getElementById(
+  "button-search-player"
+) as HTMLButtonElement;
+const containerPlayer = document.getElementById(
+  "container-player"
+) as HTMLDivElement;
+const playerImage = document.querySelector(
+  "#player-image img"
+) as HTMLImageElement;
+const playerCharacteristics = document.querySelectorAll(
+  "#player-characteristics span strong"
+);
+const playerName = document.getElementById("player-name")!;
+const playerNumber = document.getElementById("player-number")!;
+const playerAge = document.getElementById("player-age")!;
+const playerTeam = document.getElementById("player-team")!;
+
+// Function to select sport:
+function active() {
+  containerSports.classList.toggle("active");
+}
+
+// API:
+const API_URL = "https://apiv3.apifootball.com/";
+const API_KEY =
+  "7a12383b8a187e43f23ef4b4dc0157c0a0018317478883d63a92b2c47dde2426";
+
+// Function to fetch data player:
+async function fetchDataPlayer(namePlayer: string) {
+  try {
+    const response = await fetch(
+      `${API_URL}?action=get_players&player_name=${namePlayer}&APIkey=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    if (data.error === 404) return;
+
+    return data;
+  } catch {
+    console.log("Some error!");
+  }
+}
+
+buttonSearchPlayer.addEventListener("click", () => {
+  fetchDataPlayer(inputSearch.value).then((data) => {
+    let playerData = data;
+    playerData.forEach((item: IPlayer) => {
+      playerImage.src = item.player_image;
+      playerName.textContent = item.player_name;
+      playerNumber.textContent = item.player_number;
+      playerAge.textContent = item.player_age;
+      playerTeam.textContent = item.team_name;
+    });
+  });
+});
